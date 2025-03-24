6788f60274c89f8eb4625508c0b73bf5f8167b3f
Henri Dohmen <henri.dohmen@stud.tu-darmstadt.de>
2025-03-24 18:43:14 +0100

    remove old vpn

--- mod/network.nix
+++ mod/network.nix
@@ -46,40 +46,5 @@
         password-flags = 0;
       };
     };
-
-    "thielelab" = {
-      connection = {
-        autoconnect = "false";
-        id = "thielelab";
-        type = "vpn";
-      };
-      ipv4 = {
-        method = "auto";
-      };
-      ipv6 = {
-        addr-gen-mode = "stable-privacy";
-        method = "auto";
-      };
-      vpn = {
-        authtype = "password";
-        autoconnect-flags = "0";
-        certsigs-flags = "0";
-        cookie-flags = "2";
-        disable_udp = "no";
-        enable_csd_trojan = "no";
-        gateway = "fwchemie4.net.hrz.tu-darmstadt.de";
-        gateway-flags = "2";
-        gwcert-flags = "2";
-        lasthost-flags = "0";
-        pem_passphrase_fsid = "no";
-        prevent_invalid_cert = "no";
-        protocol = "anyconnect";
-        resolve-flags = "2";
-        service-type = "org.freedesktop.NetworkManager.openconnect";
-        stoken_source = "disabled";
-        xmlconfig-flags = "0";
-        password-flags = 0;
-      };
-    };
   };
 }

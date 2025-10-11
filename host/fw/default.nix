71bcb4e0f120cd3111d46f28b49932d2d17d9e89
Henri Dohmen <henridohmen@posteo.com>
2025-10-11 22:36:58 +0200

    fix framework mic

--- host/fw/default.nix
+++ host/fw/default.nix
@@ -19,6 +19,13 @@
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
+  # https://github.com/NixOS/nixos-hardware/issues/1603
+  services.pipewire.wireplumber.extraConfig.no-ucm = {
+    "monitor.alsa.properties" = {
+      "alsa.use-ucm" = false;
+    };
+  };
+
   environment.systemPackages = [
     pkgs.sbctl
   ];

6be6f1a8135ee8abe3826e352d96633f1fc1dea0
Henri Dohmen <henridohmen@posteo.com>
2026-02-06 20:14:54 +0100

    backup

--- host/roam/backup.nix
+++ host/roam/backup.nix
@@ -4,39 +4,36 @@
   secrets,
   ...
 }:
-# {
-#   age.secrets.roam-rclone-conf = {
-#     file = secrets.roam."rclone-conf.age";
-#     mode = "440";
-#     owner = "root";
-#     group = "root";
-#   };
-#
-#   systemd = {
-#     timers."backup-rclone" = {
-#       wantedBy = [ "timers.target" ];
-#       timerConfig = {
-#         OnCalendar = "daily";
-#         Persistent = true;
-#         Unit = "backup-rclone.service";
-#       };
-#     };
-#     services."backup-rclone" =
-#       let
-#         conf = config.age.secrets.roam-rclone-conf.path;
-#       in
-#       {
-#         script = ''
-#           ${pkgs.rclone}/bin/rclone --config ${conf} copy /home/hd/Documents odc:Documents
-#           ${pkgs.rclone}/bin/rclone --config ${conf} copy /git odc:git
-#         '';
-#         path = [ pkgs.rclone ];
-#         serviceConfig = {
-#           Type = "oneshot";
-#           User = "root";
-#         };
-#       };
-#   };
-# }
 {
-} # TODO: Backups for new Syncthing solution
+  age.secrets.roam-rclone-conf = {
+    file = secrets.roam."rclone-conf.age";
+    mode = "440";
+    owner = "root";
+    group = "root";
+  };
+  systemd = {
+    timers."backup-rclone" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        Unit = "backup-rclone.service";
+      };
+    };
+    services."backup-rclone" =
+      let
+        conf = config.age.secrets.roam-rclone-conf.path;
+      in
+      {
+        script = ''
+          ${pkgs.rclone}/bin/rclone --config ${conf} copy /data/sync/documents-hd onedrive:sync
+          ${pkgs.rclone}/bin/rclone --config ${conf} copy /git odc:git
+        '';
+        path = [ pkgs.rclone ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
+  };
+}
